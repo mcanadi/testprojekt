d51e82345928d1d529455cb438d72979bdcc699b
Marko Canadi <marko.canadi@gg-grupa.eu>
2016-10-04 09:57:50 +0200

    button name
    changed

--- testprojekt/Form1.Designer.cs
+++ testprojekt/Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.Goran1 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.button4marko = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -71,21 +71,21 @@
             this.Goran1.Text = "Goran1";
             this.Goran1.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // button4marko
             // 
-            this.button4.Location = new System.Drawing.Point(292, 168);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "MARKO";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4marko.Location = new System.Drawing.Point(292, 168);
+            this.button4marko.Name = "button4marko";
+            this.button4marko.Size = new System.Drawing.Size(75, 23);
+            this.button4marko.TabIndex = 4;
+            this.button4marko.Text = "MARKO";
+            this.button4marko.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(455, 237);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button4marko);
             this.Controls.Add(this.Goran1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -102,7 +102,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button Goran1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button4marko;
     }
 }
 
